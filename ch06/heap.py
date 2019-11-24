212c19cd8cd3cfe6e873b85c43d32cc331fa464c
shuxiangguo <shuxiangguo_li@163.com>
2019-11-24 16:06:20 +0800

    最小堆实现的优先队列

---
+++ ch06/heap.py
@@ -0,0 +1,59 @@
+# 利用二叉堆实现优先队列（最小堆）
+class Heap:
+    def __init__(self):
+        # 列表里的0为了使后续的方法可以使用整数除法
+        self.heapList = [0]
+        self.currentSize = 0
+
+    # 向上调整
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]:
+                tmp = self.heapList[i//2]
+                self.heapList[i//2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i // 2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+
+    # 向下调整
+    def percDown(self, i):
+        while i * 2 <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i*2
+        else:
+            if self.heapList[i*2] < self.heapList[i*2+1]:
+                return i*2
+            else:
+                return i*2 + 1
+
+    # 删除最小值并返回
+    def delMin(self):
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+
+    # 根据元素列表构建堆
+    def buildHeap(self, alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        while i > 0:
+            self.percDown(i)
+            i = i - 1
+
+
